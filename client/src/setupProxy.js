d1b86f321bcda86d1ab194b2a4988c201e897f3c
Rohan Ravindran <rohanravindran@Rohans-MacBook-Pro-3.local>
2021-08-12 01:13:25 -0400

    deleted package-lock.json

--- client/src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
-module.exports = (app) => {
-  app.use(
-    ["/api", "/auth/questrade"],
-    createProxyMiddleware({
-      target: "http://localhost:5000",
-    })
-  );
-};
